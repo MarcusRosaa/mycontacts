625c1bd02c80b5dd3d733ef8cedc4cb85b9e12f0
MarcusRosaa <marcusvrosa1@gmail.com>
2022-01-07 20:33:44 -0300

    init commit api

---
+++ api/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 13,
+  },
+  rules: {
+    'class-methods-use-this': 'off',
+    'consistent-return': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+  },
+};
---
+++ api/src/app/controllers/ContactController.js
@@ -0,0 +1,82 @@
+const ContactsRepository = require('../repositories/ContactsRepository');
+
+class ContactController {
+  async index(request, response) {
+    const { orderBy } = request.query;
+    const contacts = await ContactsRepository.findAll(orderBy);
+
+    response.json(contacts);
+  }
+
+  async show(request, response) {
+    const { id } = request.params;
+
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    response.json(contact);
+  }
+
+  async store(request, response) {
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactsExists = await ContactsRepository.findByEmail(email);
+
+    if (contactsExists) {
+      return response.status(400).json({ error: 'Email already in use' });
+    }
+
+    const contact = await ContactsRepository.create({
+      name, email, phone, category_id,
+    });
+
+    response.json(contact);
+  }
+
+  async update(request, response) {
+    const { id } = request.params;
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    const contactsExists = await ContactsRepository.findById(id);
+
+    if (!contactsExists) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactsEmailExists = await ContactsRepository.findByEmail(email);
+    if (contactsEmailExists && contactsEmailExists.id !== id) {
+      return response.status(400).json({ error: 'Email already in use' });
+    }
+
+    const contact = await ContactsRepository.update(id, {
+      name, email, phone, category_id,
+    });
+
+    response.json(contact);
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    await ContactsRepository.delete(id);
+    // 204: no content
+    response.sendStatus(204);
+  }
+}
+
+module.exports = new ContactController();
