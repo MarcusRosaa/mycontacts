4db2da20b30e94188195f58b2dfd7dfb8503ed6e
MarcusRosaa <marcusvrosa1@gmail.com>
2022-01-12 18:36:20 -0300

    resolve porta para back

--- api/src/index.js
+++ api/src/index.js
@@ -12,4 +12,4 @@ app.use((error, request, response, next) => {
   response.sendStatus(500);
 });
 
-app.listen(3000, () => console.log('RODANDO EM http://localhost:3000'));
+app.listen(3001, () => console.log('RODANDO EM http://localhost:3000'));
