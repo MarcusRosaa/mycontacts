a8e110ef80127025849217a002442eb597fb879f
MarcusRosaa <marcusvrosa1@gmail.com>
2022-01-17 22:02:49 -0300

    adiciona metodo de delete e update de categorias

--- api/src/routes.js
+++ api/src/routes.js
@@ -12,6 +12,9 @@ router.post('/contacts', ContactController.store);
 router.put('/contacts/:id', ContactController.update);
 
 router.get('/categories', CategoryController.index);
+router.get('/categories/:id', CategoryController.show);
 router.post('/categories', CategoryController.store);
+router.put('/categories/:id', CategoryController.update);
+router.delete('/categories/:id', CategoryController.delete);
 
 module.exports = router;
